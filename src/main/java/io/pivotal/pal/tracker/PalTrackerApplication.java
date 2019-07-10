8ee628884e5eb714b427d8b17d0b82648f77d6f7
Tom Maher <tdmaher@gmail.com>
2019-07-10 15:11:06 -0600

    changed the conditional from missingbean to missingclass

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -2,16 +2,29 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
+
+
     @Bean
-    public TimeEntryRepository timeEntryRepository(DataSource datasource){
-        //return new InMemoryTimeEntryRepository();
+    @ConditionalOnClass(DataSource.class)
+    @ConditionalOnProperty()
+    public TimeEntryRepository jdbcRepository(DataSource datasource){
         return new JdbcTimeEntryRepository(datasource);
+    }
+
+
+    @Bean
+    @ConditionalOnMissingClass("javax.sql.DataSource")
+    public TimeEntryRepository timeEntryRepository() {
+        return new InMemoryTimeEntryRepository();
     }
 
 
